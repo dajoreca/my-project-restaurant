98a70015c8c6e88b04fd8419592cefd667b39335
Daniel Reollo <dreollo92@gmail.com>
2022-05-30 12:20:01 +0200

    useFormik

--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const NuevoPlatillo = () => {
+
+
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            precio:'',
+            categoria:'',
+            imagen:'',
+            descripcion:'',
+        },
+        onSubmit: datos => {
+            console.log(datos);
+        }
+    })
+
+
+
     return ( 
         <>
             <h1 className="text-3xl font-light mb-4">Agregar Platillo</h1>
             
             <div className= "flex justify-center mt-10">
                 <div className="w-full max-w-3xl ">
-                    <form>
+                    <form
+                        onSubmit={formik.handleSubmit}
+                    >
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">Nombre</label>
                             <input 
@@ -15,6 +35,8 @@ const NuevoPlatillo = () => {
                                 id="nombre"
                                 type="text"
                                 placeholder="Nombre Platillo"
+                                value={formik.values.nombre}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         <div className="mb-4">
@@ -25,6 +47,8 @@ const NuevoPlatillo = () => {
                                 type="number"
                                 placeholder="Ej. 100 €"
                                 min="0"
+                                value={formik.values.precio}
+                                onChange={formik.handleChange}
                             />
                         </div>
                         <div className="mb-4">
@@ -33,6 +57,8 @@ const NuevoPlatillo = () => {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="categoria"
                                 name="categoria"
+                                value={formik.values.categoria}
+                                onChange={formik.handleChange}
                             >
                                 <option value="">-- Seleccione --</option>
                                 <option value="desayuno">Desayuno</option>
@@ -51,6 +77,8 @@ const NuevoPlatillo = () => {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                                 id="imagen"
                                 type="file"
+                                value={formik.values.imagen}
+                                onChange={formik.handleChange}
 
                             />
                         </div>
@@ -60,6 +88,8 @@ const NuevoPlatillo = () => {
                                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-40"
                                 id="descripcion"
                                 placeholder="Descripción del Platillo"
+                                value={formik.values.descripcion}
+                                onChange={formik.handleChange}
                             ></ textarea>
                         </div>
 
