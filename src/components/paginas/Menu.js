166a5cd72cd90dc4276f09897220aa39ad44e3ca
Daniel Reollo <dreollo92@gmail.com>
2022-05-30 12:08:04 +0200

    Formulario

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -5,7 +5,7 @@ const Menu = () => {
     return ( 
         <>
             <h1 className="text-3xl font-light mb-4">Menu</h1>
-            <Link to="nuevo-platillo" className="bg-blue-800 hover:bg-blue-500 incline-block mb-5 p-2 text-white uppercase font-bold">
+            <Link to="/nuevo-platillo" className="bg-blue-800 hover:bg-blue-500 incline-block mb-5 p-2 text-white uppercase font-bold">
                 Agregar platillo
             </Link>
         </>
